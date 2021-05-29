5d00e3222e35465c6397a67fd5a3c0e4ff2ebdf2
revaxl <robin208@hotmail.fr>
2021-05-30 02:31:13 +0900

    remove dependency on external mod

--- src/mod.js
+++ src/mod.js
@@ -2,18 +2,16 @@
  * authors: Revingly
 */
 
-const coreMod = CoreMod.mod;
-
 class MICC {
-    constructor() {
-        this.mod = "Revingly-MICC";
-        Logger.info(`Loading: ${this.mod}`);
-        ModLoader.onLoad[this.mod] = this.Start.bind(this);
-    }
+    static MOD_NAME = "Revingly-MICC";
 
-    Start() {
+    static Start() {
+        Logger.info(`Loading: ${MICC.MOD_NAME}`);
         const MEDS = "543be5664bdc2dd4348b4569";
         const SMALL_SICC_ID = "5d235bb686f77443f4331278";
+
+        const handbook = DatabaseServer.tables.templates.handbook.Items;
+        const locales = DatabaseServer.tables.locales.global;
 
         const itemId = "Revingly_MICC",
             itemCategory = "5795f317245977243854e041",
@@ -22,10 +20,11 @@ class MICC {
             itemName = "Medical SICC",
             itemShortName = "MICC",
             itemDescription = "SICC for med items",
-            itemTrader = "54cb57776803fa99248b456e", //Thrapist
+            itemTrader = "54cb57776803fa99248b456e", //Therapist
             itemTraderPrice = 100000,
-            itemTraderCurrency = "RUB",
             itemTraderLV = 1;
+
+        const therapist = DatabaseServer.tables.traders[itemTrader];
 
         const item = JsonUtil.clone(DatabaseServer.tables.templates.items[SMALL_SICC_ID]);
         item._id = itemId;
@@ -35,15 +34,47 @@ class MICC {
         item._props.Grids[0]._props.cellsV = 4;
         DatabaseServer.tables.templates.items[itemId] = item;
 
-        coreMod.CreateNewItemLocale("en", itemId, itemName, itemShortName, itemDescription);
-        coreMod.CreateHandbookItem(itemId, itemCategory, itemFleaPrice);
-        coreMod.CreateTraderAssort(itemId, itemId, itemTrader, itemTraderPrice, itemTraderCurrency, itemTraderLV, true);
-        coreMod.CreateTraderAssort(itemId, itemId, "ragfair", itemTraderPrice, "RUB", 1, true);
+        // add the item to the trader
+        for (const locale in locales) {
+            locales[locale].templates[itemId] =
+            {
+                "Name": itemName,
+                "ShortName": itemShortName,
+                "Description": itemDescription
+            };
+        }
+        handbook.push(
+            {
+                "Id": itemId,
+                "ParentId": itemCategory,
+                "Price": itemFleaPrice
+            }
+        );
+        therapist.assort.items.push({
+            "_id": itemId,
+            "_tpl": itemId,
+            "parentId": "hideout",
+            "slotId": "hideout",
+            "upd":
+            {
+                "UnlimitedCount": true,
+                "StackObjectsCount": 999999
+            }
+        });
+        therapist.assort.barter_scheme[itemId] = [
+            [
+                {
+                    "count": itemTraderPrice,
+                    "_tpl": "5449016a4bdc2d6f028b456f" // roubles
+                }
+            ]
+        ];
+        therapist.assort.loyal_level_items[itemId] = itemTraderLV;
 
-        this.allowMiccIntoSecureContainers(itemId);
+        MICC.allowMiccIntoSecureContainers(itemId);
     }
 
-    allowMiccIntoSecureContainers(itemId) {
+    static allowMiccIntoSecureContainers(itemId) {
         // nootropix mod for secure containers conflict with this mod, so we try to make a compromise
         const NOOTROPIX_MOD = "Nootropix-BigSecureCases";
         const secureContainers = {
@@ -55,18 +86,32 @@ class MICC {
             "waistPouch": "5732ee6a24597719ae0c0281"
         };
 
-        if (coreMod.IsModLoaded(NOOTROPIX_MOD)) {
+        if (ModLoader.onLoad[NOOTROPIX_MOD] !== undefined) { // check if big secure cases is added to modloader
+            Logger.debug(`${NOOTROPIX_MOD} detected`);
             const configFilePath = `${ModLoader.getModPath(NOOTROPIX_MOD)}config/config.json`;
             const { cases } = JsonUtil.deserialize(VFS.readFile(configFilePath));
 
             for (const secureCase in cases) {
                 if (secureCase.removeFilters) continue;
 
-                DatabaseServer.tables.templates.items[secureContainers[secureCase]]._props.Grids[0]._props.filters[0].Filter.push(itemId)
+                DatabaseServer.tables.templates.items[secureContainers[secureCase]]
+                    ._props
+                    .Grids[0]
+                    ._props
+                    .filters[0]
+                    .Filter
+                    .push(itemId)
             }
         } else {
+            Logger.debug(`${NOOTROPIX_MOD} not detected`);
             for (const secureCase in secureContainers) {
-                DatabaseServer.tables.templates.items[secureContainers[secureCase]]._props.Grids[0]._props.filters[0].Filter.push(itemId)
+                DatabaseServer.tables.templates.items[secureContainers[secureCase]]
+                    ._props
+                    .Grids[0]
+                    ._props
+                    .filters[0]
+                    .Filter
+                    .push(itemId)
             }
         }
     }
